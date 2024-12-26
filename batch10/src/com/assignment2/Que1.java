115ceb13ece4bd2a4e06e3edd3a97940aac19302
DEX <DEX@DESKTOP-F64DVA1>
2024-12-26 06:27:46 -0800

    Assigment2

---
+++ batch10/src/com/assignment2/Que1.java
@@ -0,0 +1,18 @@
+package com.assignment2;
+
+public class Que1 {
+	public static void main (String[] args) {
+		int a;
+		a = 10;
+		System.out.println(a);
+		boolean  flag = true;
+		System.out.println(flag);
+		char myletter = 'R';
+		System.out.println(myletter);
+		float  mynum = 8.96f;
+		System.out.println(mynum);
+	}
+	
+	
+
+}
