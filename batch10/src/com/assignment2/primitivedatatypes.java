4883e0d3192f1be80a6f83f10cabcc57e01e6b8c
Priyanka <Priyanka@LAPTOP-BKHVGJOE>
2024-12-26 15:04:01 +0530

    assignment 2  declares different primitive data types

---
+++ batch10/src/com/assignment2/primitivedatatypes.java
@@ -0,0 +1,36 @@
+package com.assignment2;
+
+public class primitivedatatypes {
+	public static void main (String[] args)
+	
+	{
+		//integer type
+		byte myByte = 100;
+		short myShort = 22000;
+		int myInt = 134357;
+		long myLong = 1366778;
+		
+		//floating point
+		float myFloat = 10.11f;
+		double myDouble = 123.876;
+		
+		//character type
+		char myChar = 'A';
+		 
+		//Boolean type
+		boolean myBoolean = true;
+		
+		//print the value
+		System.out.println("byte value:" + myByte);
+		System.out.println("short value:" + myShort);
+		System.out.println("int value:" + myInt);
+		System.out.println("long value:" + myLong);
+		System.out.println("float value:" + myFloat);
+		System.out.println("double value:" + myDouble);
+		System.out.println("char value:" + myChar);
+		System.out.println("boolean value:" + myBoolean);
+		
+		
+	}
+
+}
