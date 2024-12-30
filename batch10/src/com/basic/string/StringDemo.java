48938894f4b21e2cc78a4f32347c7f1d737368cf
swapn <swapn@LAPTOP-BJNVAM67.lan>
2024-12-30 09:05:24 +0530

    Strings

---
+++ batch10/src/com/basic/string/StringDemo.java
@@ -0,0 +1,17 @@
+package com.basic.string;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+
+		String s1 = "Code";
+
+		String s2 = new String("Coverage");
+
+		s1.concat(s2);
+
+		System.out.println(s1);
+
+	}
+
+}
