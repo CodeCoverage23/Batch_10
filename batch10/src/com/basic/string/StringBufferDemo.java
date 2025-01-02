2c7e88c1c8c20b48e21318201dcccccbd2c12d7c
TUSHAR <chavantushar218@gmail.com>
2025-01-02 19:03:35 +0530

    String!!

--- batch10/src/com/basic/string/StringBufferDemo.java
+++ batch10/src/com/basic/string/StringBufferDemo.java
@@ -3,8 +3,11 @@ package com.basic.string;
 public class StringBufferDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		StringBuffer sb = new StringBuffer("Hello");
+		sb.append(" World!");
+		System.out.println(sb);
 
 	}
+		
 
 }
---
+++ batch10/src/com/basic/string/StringBuilderDemo.java
@@ -0,0 +1,14 @@
+package com.basic.string;
+
+public class StringBuilderDemo {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder("Hello");
+		sb.append(" World!");
+		System.out.println(sb);
+		sb.insert(5, " Java");
+		System.out.println(sb);
+		sb.delete(5, 10);
+		System.out.println(sb);
+
+	}
+}
