3a56c97c4e3c96e24f835de90f17f4ffc37a195b
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2025-01-02 17:38:37 +0530

    Assignment 3

---
+++ batch10/src/com/assignment_3/basic/Assignment_3.java
@@ -0,0 +1,20 @@
+package com.assignment_3.basic;
+
+/*
+ * 1. Design the separate method as String getStudentName (String name)
+ *  which return student name and print it.
+
+ */
+
+public class Assignment_3 {
+	public static String getStudentName(String name) {
+		return name;
+		}
+
+	public static void main(String[] args) {
+		
+		String studentName = getStudentName("Pallavi Ghodke");
+		System.out.println("Student Name: " +studentName);
+		}
+
+}
