083ffbefd0d32cc56a3139e3d421236eb42f00f9
DEX <DEX@DESKTOP-F64DVA1>
2025-01-02 16:23:53 +0530

    commit

---
+++ batch10/src/com/Assignment4/Que3.java
@@ -0,0 +1,21 @@
+package com.Assignment4;
+//Multiplication
+
+public class Que3 {
+
+	
+    public static void main(String[] args) {
+        int n1 = 5;  
+        int n2 = 4;  
+
+        
+        int result = multiply(n1, n2);
+
+        // Print the result in the main method
+        System.out.println("The result of multiplying " + n1 + " and " + n2 + " is: " + result);
+    }
+    public static int multiply(int n1, int n2) {
+        return n1 * n2; 
+    }
+
+}
