27f53dadacce844ed3b24e6c64af507887c0dad4
Neha Thakur <2nthakur2580@gmail.com>
2025-01-02 22:42:23 +0530

    Assignment1

---
+++ batch10/src/com/assignment1/Que_1.java
@@ -0,0 +1,13 @@
+package com.assignment1;
+
+public class Que_1 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		System.out.println("Code Coverage");
+		
+
+	}
+
+}
