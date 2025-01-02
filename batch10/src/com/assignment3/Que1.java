9ac16b2f0f3d4c7721c26dfb782fc1c8bb51afb2
DEX <DEX@DESKTOP-F64DVA1>
2025-01-02 16:22:43 +0530

    Assign3

---
+++ batch10/src/com/assignment3/Que1.java
@@ -0,0 +1,17 @@
+package com.assignment3;
+
+public class Que1 { 
+	public static void main (String[] args) {
+		
+		
+		String name = Que1.getStudentName("Mrunali");
+		System.out.println("My name is " +  name);
+	}
+	public static String getStudentName(String name) {
+		
+		String studentName = name;
+		return studentName;
+			
+	}
+
+}
