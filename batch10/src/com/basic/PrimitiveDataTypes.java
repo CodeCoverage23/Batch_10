2a1587bb0376bc72035bbd5065b71a5d70dedaec
user <user@swamiji>
2025-01-03 22:14:14 +0530

    Assignment 2.1

---
+++ batch10/src/com/basic/PrimitiveDataTypes.java
@@ -0,0 +1,25 @@
+package com.basic;
+
+public class PrimitiveDataTypes {
+	public static void main(String [] args) {
+		 int iVar;
+		 float fVar;
+		final byte bVar;
+		char cVar ;
+		boolean boVar;
+		long lVar;
+		double dVar;
+		short sVar;
+		
+		//System.out.println("Default Value of Integer is :" + iVar);
+		//System.out.println("Default Value of Float is :" +fVar);
+		//System.out.println("Default Value of Byte is :" +bVar);
+		//System.out.println("Default Value of Character is :" +cVar);
+		//System.out.println("Default Value of Boolean is :" +boVar);
+		//System.out.println("Default Value of Long is :" +lVar);
+		//System.out.println("Default Value of Double is :" +dVar);
+		//System.out.println("Default Value of Short is :" +sVar);
+		System.out.println("Thank You");
+	}
+
+}
