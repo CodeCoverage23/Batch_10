531fc00a739b41e656f5104426a51595c2ea6f08
user <user@swamiji>
2025-01-03 23:02:16 +0530

    Assignment 4

---
+++ batch10/src/com/basic/Multiplication.java
@@ -0,0 +1,17 @@
+package com.basic;
+
+public class Multiplication {
+	public static int multiplyNumbers(int num1 , int num2) {
+		return num1*num2;
+	}
+		public static void main(String[] args) {
+		
+	int num1 = 10;
+	int num2 = 10;
+	 int multiply = multiplyNumbers(num1, num2);
+	 
+
+	System.out.println("The Multiplication of "+ num1 + " and "+ num2 +" is  " + multiply);
+		}
+	
+}
---
+++ batch10/src/com/basic/Substraction.java
@@ -0,0 +1,17 @@
+package com.basic;
+
+public class Substraction {
+
+	public static int addition(int num1 , int num2) {
+		return num1+num2;
+	}
+		public static void main(String[] args) {
+		
+	int num1 = 20;
+	int num2 = 10;
+	 int sum = addition(num1, num2);
+	 
+
+	System.out.println("The SUM of "+ num1 + " and "+ num2 +" is  " + sum);
+		}
+}
