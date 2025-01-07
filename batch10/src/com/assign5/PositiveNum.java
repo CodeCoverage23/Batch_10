02a0c05d7af28dd68951ed9f5e13f89df17edb5f
user <user@swamiji>
2025-01-07 22:52:04 +0530

    assignment 5.1

---
+++ batch10/src/com/assign5/PositiveNum.java
@@ -0,0 +1,13 @@
+package com.assign5;
+
+public class PositiveNum {
+public static void main(String [] args) {
+	int s = 2;
+	if(s % 2 == 0) {
+		System.out.println("The number is Positive");
+	}
+	else {
+		System.out.println("The number is negative Number");
+	}
+}
+}
