610cbd7d3d7da67bbfbde35a2ad357ba1057c1e7
Neha-Thakur-10 <2nthakur2580@gmail.com>
2025-01-15 23:28:06 +0530

    Update Que8.java

--- batch10/src/com/assignment5/Que8.java
+++ batch10/src/com/assignment5/Que8.java
@@ -1,5 +1,22 @@
+//8. Write a program to swap the two numbers.
 package com.assignment5;
 
-public class Que8 {
+import java.util.Scanner;
 
+public class Que8 {
+	 static void swap(int a,int b) {
+		 System.out.println("Given Number are: "+a+ " " +b);
+		int temp=a;
+		a=b;
+		b=temp;
+		 System.out.println("After swapping Numbers are: "+a+ " " +b);
+	 }
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the two numbers: ");
+		int a =sc.nextInt();
+		int b =sc.nextInt();
+		
+		swap(a,b);
+	}
 }
