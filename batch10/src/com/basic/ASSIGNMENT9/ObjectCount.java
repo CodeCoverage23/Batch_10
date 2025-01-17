d67dffdb15cefefe4d9e9ca662b953d3dfda478e
user <user@swamiji>
2025-01-17 21:40:37 +0530

    assignment 9

---
+++ batch10/src/com/basic/ASSIGNMENT9/ObjectCount.java
@@ -0,0 +1,17 @@
+package com.basic.ASSIGNMENT9;
+
+public class ObjectCount {
+static int count = 0;
+
+public ObjectCount()
+{
+	count++;
+}
+
+public static void main(String [] args) {
+	ObjectCount o1 = new ObjectCount();
+	ObjectCount o2 = new ObjectCount();
+	ObjectCount o3 = new ObjectCount();
+	System.out.println("The number of object is: " + o3.count);
+}
+}
