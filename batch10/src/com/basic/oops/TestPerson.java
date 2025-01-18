474ad3f354a82524eba11de92bf75c6b8312236f
TUSHAR <chavantushar218@gmail.com>
2025-01-18 18:55:01 +0530

    Encapsulation

---
+++ batch10/src/com/basic/oops/TestPerson.java
@@ -0,0 +1,16 @@
+package com.basic.oops;
+
+public class TestPerson {
+
+	public static void main(String[] args) {
+		Person p = new Person();
+		p.setName("Tushar");
+		p.setAge(23);
+		p.setAddress("pune");
+		p.setMobileNumber(999999999);
+		
+		System.out.println(p);
+
+	}
+
+}
