c7ac90dc7eccc491fe4ca219b1fa3f9bf298f625
Priyanka <Priyanka@LAPTOP-BKHVGJOE>
2025-02-02 15:06:39 +0530

    print first character of your name

---
+++ batch10/src/com/assignment2/FirstCharacter.java
@@ -0,0 +1,10 @@
+package com.assignment2;
+
+public class FirstCharacter {
+	public static void main(String[]args) {
+		//name is priyanka and below we print frist character of priyanka
+		char ch = 'p';
+		System.out.println("First character is>>"+ch);
+	}
+
+}
