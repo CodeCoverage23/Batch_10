224ccf3e17df49ee77e33408faabe19ab81de921
Priyanka <Priyanka@LAPTOP-BKHVGJOE>
2025-02-02 15:25:50 +0530

    global variable declaration

---
+++ batch10/src/com/assignment2/globalvariable.java
@@ -0,0 +1,11 @@
+package com.assignment2;
+
+public class globalvariable {
+
+	public static void main(String[] args) {
+		int y=20; //global variable declaration
+		System.out.println("print the value of>>"+y);
+
+	}
+
+}
