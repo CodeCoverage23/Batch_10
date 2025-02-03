3aeda1d8efb33892e68533fcbb15727737d2e0b6
Priyanka <Priyanka@LAPTOP-BKHVGJOE>
2025-02-03 13:37:42 +0530

    calculating the total average marks of students

---
+++ batch10/src/com/assignment4/Subjects.java
@@ -0,0 +1,19 @@
+package com.assignment4;
+
+public class Subjects {
+
+	public static void main(String[] args) {
+	int maths=90;
+	int hindi=90;
+	int english=95;
+	int history=80;
+	int oops=90;
+	int total=maths+hindi+english+history+oops;
+	float avg = total/5;//calculating average
+	System.out.println("Total of five subject marks:"+ total);
+	System.out.println("Average of five subject marks:"+ avg);
+	
+
+	}
+
+}
