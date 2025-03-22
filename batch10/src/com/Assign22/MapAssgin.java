0c45c4a1048ddd76a701e1bbd2b666cb4d7cd217
TUSHAR <chavantushar218@gmail.com>
2025-03-23 02:00:26 +0530

    ASSIGN

---
+++ batch10/src/com/Assign22/MapAssgin.java
@@ -0,0 +1,22 @@
+package com.Assign22;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapAssgin {
+
+	public static void main(String[] args) {
+		
+		Map<Integer, String> map = new HashMap<>();
+		
+		map.put(3, "Ram");
+		map.put(2, "Sohan");
+		map.put(1, "Ashok");
+		map.put(6, "Ajay");
+		map.put(5, "Prasnna");
+		
+		System.out.println(map);
+
+	}
+
+}
