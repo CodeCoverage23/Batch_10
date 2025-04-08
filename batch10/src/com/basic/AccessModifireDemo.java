36f158c3f40566e7edb026bf37e90d2d8f911075
TUSHAR <chavantushar218@gmail.com>
2025-04-09 00:38:09 +0530

    Basic

---
+++ batch10/src/com/basic/AccessModifireDemo.java
@@ -0,0 +1,23 @@
+package com.basic;
+
+public class AccessModifireDemo {
+
+
+		
+		private void display() {
+			System.out.println("Private Method");
+		}
+		protected void name() {
+			System.out.println("Protected method");
+		}
+		
+		void print() {
+			System.out.println("Default Method");
+		}
+
+		public void show() {
+			System.out.println("Public Method");
+		}
+	
+
+}
